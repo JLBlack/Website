5958d16098fa0b3b26b9fb7f01f8447eea281978
Shana Abell <farsi.strada@gmail.com>
2020-06-07 22:43:17 -0700

    DAT DEFAULT ACL

--- website/core/settings/production.py
+++ website/core/settings/production.py
@@ -17,8 +17,9 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_DEFAULT_ACL = 'public-read'
 AWS_STORAGE_BUCKET_NAME = 'cdn.jpnerdbio.com'
-# AWS_S3_REGION_NAME = 'us-west-2'
+AWS_S3_REGION_NAME = 'us-west-2'
 AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
